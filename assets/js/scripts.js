0bd5c5d6112626206b9f7d3a09ff81c418d62756
Javan Oyugi <jooyugi@safaricom.co.ke>
2022-10-27 15:19:05 +0300

    add submit button to form

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,30 @@
-$(document).ready(function () {
-  $("#design-description").hide();
-  $("#dev-description").hide();
-  $("#product-description").hide();
-  
-  $("#design-icon").click(function () {
-    $(this).toggle();
+$(document).ready(() => {
+
+  $(".icon-description").hide();
+
+  $("#design-icon").click(event => {
+    $(event.currentTarget).toggle();
     $("#design-description").toggle();
   });
-  $("#dev-icon").click(function () {
-    $(this).toggle();
+  $("#dev-icon").click(event => {
+    $(event.currentTarget).toggle();
     $("#dev-description").toggle();
   });
-  $("#product-icon").click(function () {
-    $(this).toggle();
+  $("#product-icon").click(event => {
+    $(event.currentTarget).toggle();
     $("#product-description").toggle();
   });
 
-  $("#design-description").click(function () {
-    $(this).toggle();
+  $("#design-description").click(event => {
+    $(event.currentTarget).toggle();
     $("#design-icon").toggle();
   });
-  $("#dev-description").click(function () {
-    $(this).toggle();
+  $("#dev-description").click(event => {
+    $(event.currentTarget).toggle();
     $("#dev-icon").toggle();
   });
-  $("#product-description").click(function () {
-    $(this).toggle();
+  $("#product-description").click(event => {
+    $(event.currentTarget).toggle();
     $("#product-icon").toggle();
   });
 })
